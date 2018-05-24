730cb90bd20625edd5fb2766c0bb21c0ed7ea48b
Ray Tsang <rayt@google.com>
2018-05-24 12:19:32 +0200

    Updated configuration

--- spring-boot2-example/work-server/src/main/java/com/example/workserver/WorkServerApplication.java
+++ spring-boot2-example/work-server/src/main/java/com/example/workserver/WorkServerApplication.java
@@ -1,5 +1,6 @@
 package com.example.workserver;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.http.HttpStatus;
@@ -13,13 +14,16 @@ import java.net.URI;
 @SpringBootApplication
 @RestController
 public class WorkServerApplication {
+	@Value("${endpoint:http://localhost:8081/meet}")
+	private String endpoint;
+
 	@GetMapping("/work")
 	public String work() {
 		RestTemplate restTemplate = new RestTemplate();
 		int count = 0;
 		for (int i = 0; i < 4; i++) {
 			ResponseEntity<Void> entity = restTemplate
-					.getForEntity(URI.create("http://localhost:8081/meet"), Void.class);
+					.getForEntity(URI.create(endpoint), Void.class);
 			if (entity.getStatusCode() == HttpStatus.OK) {
 				count++;
 			}
