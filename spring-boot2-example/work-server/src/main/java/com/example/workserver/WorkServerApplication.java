ca8fd25bcbc84dec75887a37a31a75c9469818c6
Ray Tsang <rayt@google.com>
2018-05-26 15:35:49 +0300

    added additional propagation

--- spring-boot2-example/work-server/src/main/java/com/example/workserver/WorkServerApplication.java
+++ spring-boot2-example/work-server/src/main/java/com/example/workserver/WorkServerApplication.java
@@ -13,7 +13,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
-import java.io.IOException;
 import java.net.URI;
 
 @SpringBootApplication
