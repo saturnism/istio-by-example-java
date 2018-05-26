4a8194f72a7ca7c01e5d1a04fa66b537e83e78f3
Ray Tsang <rayt@google.com>
2018-05-26 15:24:29 +0300

    make sure resttemplate is wired by sleuth properly

--- spring-boot2-example/work-server/src/main/java/com/example/workserver/WorkServerApplication.java
+++ spring-boot2-example/work-server/src/main/java/com/example/workserver/WorkServerApplication.java
@@ -2,51 +2,57 @@ package com.example.workserver;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.http.client.ClientHttpResponse;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.ResponseErrorHandler;
 import org.springframework.web.client.RestTemplate;
 
 import java.io.IOException;
 import java.net.URI;
 
 @SpringBootApplication
-@RestController
 public class WorkServerApplication {
-	private static Logger logger = LoggerFactory.getLogger(WorkServerApplication.class);
-
-	@Value("${endpoint:http://localhost:8081/meet}")
-	private String endpoint;
 
 	@Bean
 	public RestTemplate restTemplate() {
 		return new RestTemplate();
 	}
 
-	@GetMapping("/work")
-	public String work(RestTemplate restTemplate) {
-		int count = 0;
-		for (int i = 0; i < 4; i++) {
-			logger.info("Going to meeting: {}", i);
-			ResponseEntity<Void> entity = restTemplate
-					.getForEntity(URI.create(endpoint), Void.class);
-			if (entity.getStatusCode() == HttpStatus.OK) {
-				count++;
-			}
-		}
-
-		return "went to " + count + " meetings";
-	}
-
 	public static void main(String[] args) {
 		SpringApplication.run(WorkServerApplication.class, args);
+	}
+
+	@RestController
+	public static class WorkController {
+		private static Logger logger = LoggerFactory.getLogger(WorkController.class);
+
+		@Value("${endpoint:http://localhost:8081/meet}")
+		private String endpoint;
+
+		@Autowired
+		private RestTemplate restTemplate;
+
+
+		@GetMapping("/work")
+		public String work() {
+			int count = 0;
+			for (int i = 0; i < 4; i++) {
+				logger.info("Going to meeting: {}", i);
+				ResponseEntity<Void> entity = restTemplate
+						.getForEntity(URI.create(endpoint), Void.class);
+				if (entity.getStatusCode() == HttpStatus.OK) {
+					count++;
+				}
+			}
+
+			return "went to " + count + " meetings";
+		}
+
 	}
 }
